11326c8a0f0d71f06c6cd43d6ce5c2ae16c56405
MattyOMonday <w.matthew.he@gmail.com>
2015-04-14 23:21:42 -0500

    Create CONFIG.py
    Used to set the file name for the command prompt and gui translations

---
+++ CONFIG.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 02 12:28:21 2015
+
+@author: Matty
+"""
+"""
+This file provides a list of file names that are used by the translator,
+command line, and gui.
+To change the tranlsation of a part of the program, set the name of
+the associated file to whichever set of translations you desire
+"""
+
+#the translations for the commands and displays used in the command line
+command_line_translations = "cmdlinewords.py"
+
+#the translations for the display elements of the gui
+gui_translations = "guiwords.py"
+
+#the translations for the python keywords
+python_translations = "keywords.py"
